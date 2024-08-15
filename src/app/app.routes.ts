d840121c7e24b48d079c142250b5d2ceed1f4d3b
MatheusCesarAmaral <matheuscesaramaral@hotmail.com>
2024-08-14 21:49:52 -0300

    add 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { UsuarioComponent } from './features/cadastros/usuario/usuario.component';
 import { EmpresaComponent } from './features/cadastros/empresa/empresa.component';
+import { SobreNosComponent } from './features/sobre-nos/sobre-nos.component';
+import { FaqComponent } from './features/faq/faq.component';
+import { TrabalheConoscoComponent } from './features/trabalhe-conosco/trabalhe-conosco.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path:'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'cad_usuario', component: UsuarioComponent},
-  {path: 'cad_empresa', component: EmpresaComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cad_usuario', component: UsuarioComponent },
+  { path: 'cad_empresa', component: EmpresaComponent },
+  { path: 'sobre-nos', component: SobreNosComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'trabalhe-conosco', component: TrabalheConoscoComponent }
 ];
