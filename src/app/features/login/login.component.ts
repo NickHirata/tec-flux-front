21f9a41c106fb7e34e54841c463fef61808a3507
MatheusCesarAmaral <matheuscesaramaral@hotmail.com>
2024-08-26 17:11:45 -0300

    tentativa login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,24 +1,62 @@
 import { Component } from '@angular/core';
-import { CheckboxModule } from 'primeng/checkbox';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { PopupComponent } from '../app-popup/app-popup.component';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    CheckboxModule,
-    ButtonModule,
-    InputTextModule,
-    RouterLink,
-    RouterOutlet
-   ],
+  imports: [ReactiveFormsModule, CommonModule, PopupComponent, NgxMaskDirective, NgxMaskPipe],
+  providers: [provideNgxMask()],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
-
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  popupMessage: string = '';
+  isError: boolean = false;
+  showPopup: boolean = false;
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.loginForm = this.fb.group({  
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.http.post('http://localhost:8081/auth/login', this.loginForm.value).subscribe(
+        (response) => {
+          this.showPopupMessage('Login com sucesso!', false);
+          this.loginForm.reset();
+        },
+        (error) => {
+          this.showPopupMessage('Usuário inválido!', true);
+        }
+      );
+    }
+  }
+
+  isFieldInvalid(field: string): boolean {
+    const control = this.loginForm.get(field);
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  markAllFieldsAsTouched() {
+    this.loginForm.markAllAsTouched();
+  }
+
+  showPopupMessage(message: string, isError: boolean) {
+    this.popupMessage = message;
+    this.isError = isError;
+    this.showPopup = true;
+
+    setTimeout(() => {
+      this.showPopup = false;
+    }, 3000);
+  }
 }
