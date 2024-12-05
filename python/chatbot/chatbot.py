0a052e2ded0552470ed3845563c4d9d01d4266cd
Matheus Cesar do Amaral <matheuscesaramaral@hotmail.com>
2024-12-04 21:02:18 -0300

    chatbot

--- python/chatbot/chatbot.py
+++ python/chatbot/chatbot.py
@@ -12,9 +12,7 @@ def gerar_resposta_informada(prompt, usuario_id):
 
         # Se for o início da conversa, adiciona uma saudação personalizada
         if prompt.lower() == "inicio":
-            info_usuario = "Nicole"
-           # info_usuario = obter_informacoes_usuario(usuario_id)
-            saudacao = f"Olá {info_usuario}, eu sou o FluxBot, seu assistente de suporte técnico! Como posso ajudar hoje?"
+            saudacao = f"Olá, eu sou o FluxBot, seu assistente de suporte técnico! Como posso ajudar hoje?"
             # Adiciona a saudação ao histórico como uma mensagem do modelo
             conversations[usuario_id].append({
                 "role": "model",
@@ -31,6 +29,7 @@ def gerar_resposta_informada(prompt, usuario_id):
         # Gera a resposta usando o modelo do Google
         chat = model.start_chat(history=conversations[usuario_id])
         resposta_modelo = chat.send_message({"parts": [{"text": prompt}]})
+        print(f"Resposta do modelo: {resposta_modelo.text}")
 
         # Adiciona a resposta ao histórico
         conversations[usuario_id].append({
